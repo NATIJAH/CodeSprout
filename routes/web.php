e9e7fda97e98faf87b4743ae6da8dfe77d0f36e4
MUHAMMAD HILMI HIJAZI BIN JAMAL <hilmihijazi01@gmail.com>
2025-11-17 13:33:55 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,15 @@ Route::prefix('teacher')->group(function () {
     Route::get('/create', [TaskController::class, 'create'])->name('teacher.create');
     Route::post('/store', [TaskController::class, 'store'])->name('teacher.store');
     // Teacher Material Routes
-    Route::get('/class/{classId}/teacher-material', [App\Http\Controllers\TeacherMaterialController::class, 'index'])->name('teacher-material.index');
-    Route::get('/class/{classId}/teacher-material/create', [App\Http\Controllers\TeacherMaterialController::class, 'create'])->name('teacher-material.create');
-    Route::post('/teacher-material', [App\Http\Controllers\TeacherMaterialController::class, 'store'])->name('teacher-material.store');
-    Route::get('/teacher-material/{id}/download', [App\Http\Controllers\TeacherMaterialController::class, 'download'])->name('teacher-material.download');
-    Route::delete('/teacher-material/{id}', [App\Http\Controllers\TeacherMaterialController::class, 'destroy'])->name('teacher-material.destroy');
+    Route::resource('teachingMaterial', TeachingMaterialController::class);
+    Route::get('/teachingMaterial/{teachingMaterial}/download', [TeachingMaterialController::class, 'download'])->name('teachingMaterial.download');
 });
 
 // Student
 Route::prefix('student')->group(function () {
     Route::get('/tasks', [StudentTaskController::class, 'index'])->name('student.tasks');
     Route::post('/tasks/submit/{id}', [StudentTaskController::class, 'submit'])->name('student.submit');
+    Route::get('/material', [TeachingMaterialController::class, 'studentView'])->name('teachingMaterial.studentView');
 });
 
 
