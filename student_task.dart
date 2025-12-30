41be674509ee8d0596bd7940d21396dde37daf9c
Karen <152505413+karenyvx@users.noreply.github.com>
2025-12-30 13:08:35 +0800

    Add files via upload

---
+++ student_task.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'student_task_list.dart';
+
+class StudentTask extends StatelessWidget {
+  const StudentTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const StudentTaskList();
+  }
+}
