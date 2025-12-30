2e61b910695de99bea2cabced629cbb28e4d89a6
Karen <152505413+karenyvx@users.noreply.github.com>
2025-12-30 13:13:21 +0800

    Add files via upload

---
+++ teacher_task.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TeacherTask extends StatelessWidget {
+  const TeacherTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("📌 Task")),
+      body: const Center(child: Text("Coming Soon!")),
+    );
+  }
+}
